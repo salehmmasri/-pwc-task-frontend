62058536a963c44db1dd67ca165827541f096c93
salehmmasri <salehalmasri494@yahoo.com>
2021-09-15 23:59:18 +0300

    added deployed backend

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -6,7 +6,7 @@ export async function fetchData(department = '', filter, page, setResult, input 
 
 
     try {
-        const response = await axios.get(`http://127.0.0.1:8000/api/v1/empolyees/?departments=${department}&${filter}=${input}&page=${page}`);
+        const response = await axios.get(`https://employee-be.herokuapp.com/api/v1/empolyees/?departments=${department}&${filter}=${input}&page=${page}`);
         setResult(response.data);
     } catch (error) {
         console.error(error);
@@ -18,7 +18,7 @@ export async function fetchDepartments() {
 
 
     try {
-        const response = await axios.get('http://127.0.0.1:8000/api/v1/department/');
+        const response = await axios.get('https://employee-be.herokuapp.com/api/v1/department/');
         return (response.data);
     } catch (error) {
         console.error(error);
